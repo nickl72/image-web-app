9600d9f7ecbe9cee3d9bc023b2943b32cb42d2ae
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-09 11:17:18 -0500

    Created main content container

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
 // Components
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Main from './components/Main'
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
