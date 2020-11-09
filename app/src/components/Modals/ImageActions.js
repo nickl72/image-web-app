747874c31966a3666fb4408b786d48cae74a854d
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-09 14:05:46 -0500

    created ImageActions modal

---
+++ app/src/components/Modals/ImageActions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImageActions = () => {
+    return (
+        <div>
+            <a href='#'>Like</a>
+            <a href='#'>Download</a>
+            <a href='#'>User</a>
+        </div>
+    )
+}
+
+export default ImageActions
