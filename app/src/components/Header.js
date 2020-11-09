f43455abbd53c2fcbce0cf2d5327addeba12f90b
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-09 11:04:26 -0500

    Created header component

---
+++ app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Images</h1>
+            <input type='text' placeholder='Search' />
+            <a href='#'>Log In</a>
+        </header>
+    )
+}
