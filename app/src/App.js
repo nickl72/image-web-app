f325db6d732f7d461f19523b67f0302682443640
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-09 10:48:58 -0500

    Created and linked footer component to app

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,11 @@
+// Components
+import Footer from './components/Footer'
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Footer />
     </div>
   );
 }
---
+++ app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href='#'>About</a>
+            <p>&copy;2020 <br/>Created by:Nick LaPointe</p>
+            <a href='#'>Report Issues</a>
+        </footer>
+    )
+}
+
+export default Footer
