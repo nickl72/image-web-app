8534479c422ff19ef49225b752c77a07097fb0b0
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-09 12:17:11 -0500

    Added images to homepage

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
 
 const Main = () => {
+    // const [images,setImages] = useState([])
+    let images = []
+    // images = []
+    // https://picsum.photos/200/300?random=1
+    // if (!images.length) {
+    //     axios.get('https://picsum.photos/v2/list?limit=10')
+    //     .then(resp => {
+    //         const imageURLS = resp.data.map(image => (image.url));
+    //         setImages(imageURLS);
+    //     })
+    // }
+    // https://picsum.photos/id/237/200/300
+    for (let i = 0; i < 8; i++) {
+        const url = `https://picsum.photos/200/300?random=${Math.floor(Math.random()*100)+1}`
+        images.push(url)
+    }
+
     return (
         <main>
+            { images && images.map(url => <img src={url} alt='' />)}
         </main>
     )
 }
