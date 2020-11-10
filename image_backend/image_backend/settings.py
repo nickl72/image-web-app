2b9749b924941c664bea6e9c3177f9b7ae83c6a1
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-10 08:55:45 -0500

    Added dotenv in django

--- image_backend/image_backend/settings.py
+++ image_backend/image_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&7m9g=d*))2qcc_q5ov-0b$%fqbmwq$417ec!bk*a$@=_m*^mt'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
