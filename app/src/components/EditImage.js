19cc255fdd1c3ede29294f508a36a2c522388852
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-11 20:04:30 -0500

    Created uploadImage api helper

--- app/src/components/EditImage.js
+++ app/src/components/EditImage.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { Edit } from '../styles/Edit';
-import { getImageById, editImage } from '../services/api_helper';
+import { getImageById, editImage, uploadImage } from '../services/api_helper';
 
 const EditImage = () => {
     const [image, setImage] = useState(null)
     const [edits, setEdits] = useState({
-        brightness: 0,
+        brightness: 1,
         blur: 0,
         red: 0,
         green: 0,
         blue: 0
     })
     if (!image) {
-        getImageById(9).then(resp => {
+        getImageById(22).then(resp => {
             setImage(resp.path)
         })
     }
@@ -29,9 +29,9 @@ const EditImage = () => {
             <div>
                 <h2>Edit</h2>
                 <h3>Brightness</h3>
-                <input type='range' />
+                <input type='range' name='brightness' min='0' max='2' step='.01' value={edits.brightness} onChange={handleChange}/>
                 <h3>Blur</h3>
-                <input type='range' />
+                <input type='range' name='blur' min='0' max='5' onChange={handleChange}/>
                 <h3>Color</h3>
                 <h4>Red: {edits.red}</h4>
                 <input type='range' name='red' min='-255' max='255' onChange={handleChange}/>
@@ -39,15 +39,22 @@ const EditImage = () => {
                 <input type='range' name='green' min='-255' max='255' onChange={handleChange}/>
                 <h4>Blue: {edits.blue}</h4>
                 <input type='range' name='blue' min='-255' max='255' onChange={handleChange}/>
+
                 <h3>Insert Image</h3>
-                <p>upload: </p><input type='text' />
+                <p>upload: </p>
+                <form onSubmit={uploadImage}>
+                    {/* <input type='text' name='title' /> */}
+                    <input type='file' name='path' />
+                    {/* <select name='creator'><option value='1'>1</option></select> */}
+                    <input type='submit' value = 'Upload' />
+                </form>
                 <p>select from library</p>
                 <input type='text' />
                 <p><a href='#'>Crop</a></p>
 
-                <p><a href='#' onClick={() => editImage(9, edits)}>download image</a></p>
+                <p><a href='#' onClick={() => editImage(22, edits)}>download image</a></p>
                 <p>Download as: </p><select>
-                    <option>JEPG</option>
+                    <option>JPEG</option>
                     <option>ASCII</option>
                 </select>
 
