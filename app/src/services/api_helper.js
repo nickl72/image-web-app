2d1765493c0549ebb52b577a73108b64ce733963
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-16 15:06:26 -0500

    removed react crop

--- app/src/services/api_helper.js
+++ app/src/services/api_helper.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api'
-    // baseURL: 'https://flow-images.herokuapp.com/api'
+    // baseURL: 'http://127.0.0.1:8000/api'
+    baseURL: 'https://flow-images.herokuapp.com/api'
 })
 
 export const registerUser = async (registerData) => {
