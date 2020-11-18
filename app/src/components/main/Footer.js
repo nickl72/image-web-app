163bffee7ce3d735316f35157cec096b76561c3e
Nick LaPointe <LaPointeNicholas@JohnDeere.com>
2020-11-17 22:05:33 -0500

    added about page

--- app/src/components/main/Footer.js
+++ app/src/components/main/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { StyledFooter } from '../../styles/Footer'
 
 const Footer = () => {
     return (
         <StyledFooter>
-            <a href='#'>About</a>
+            <Link to='/about'>About</Link>
             <p>&copy;2020 <br/>Created by Nick LaPointe</p>
             <a href='https://github.com/nickl72/image-web-app-frontend/issues' target='_blank' >Report Issues</a>
         </StyledFooter>
